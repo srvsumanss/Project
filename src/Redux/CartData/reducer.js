76a111d878ff77865d1584b7fa83e0dbd0c2dbe4
Saurabh Suman <96100046+srvsumanss@users.noreply.github.com>
2022-07-25 12:00:31 +0530

    update reducer.js

---
+++ src/Redux/CartData/reducer.js
@@ -0,0 +1,31 @@
+import {CARTDATA_LOADING , CARTDATA_SUCCESS , CARTDATA_FAILURE} from "./action" ;
+
+const initState = {
+    loading :false ,
+    cartData :[],
+    error :false ,
+}
+
+export const cartReducer = (store = initState,{type,payload})=>{
+    switch(type){
+        case CARTDATA_LOADING:
+            return{
+                ...store,
+                loading : true,
+            }
+            case CARTDATA_SUCCESS :
+                return{
+                    ...Store,
+                    loading:false,
+                    cartData:[...payload] ,
+
+                }
+                case CARTDATA_FAILURE :
+                    return{
+                        ...store,
+                        loading:false,
+                        error:true,
+                    }
+                    default:return store;
+    }
+}
